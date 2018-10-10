0a3f442c639fa5312aa49d0d57377603db2c5837
285672259@qq.com <a6470280>
2018-10-10 16:51:37 +0800

    init && dev package

---
+++ src/Http.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anshi
+ * Date: 2018/10/10
+ * Time: 16:42
+ */
+
+namespace AvalonComposerUtil;
+
+
+class Http
+{
+    public function get(){
+
+    }
+}
