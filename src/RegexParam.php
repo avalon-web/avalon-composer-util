df83cc542407a4094bc829976c9faee4ee3622d1
285672259@qq.com <a6470280>
2018-10-15 17:32:23 +0800

    update

--- src/RegexParam.php
+++ src/RegexParam.php
@@ -13,6 +13,11 @@ class RegexParam
         //找出不符合要求的参数
         $findEle = $paramsArr->find(function ($d) use ($arr) {
             ["id" => $id] = $d;
+            //如果是可选参数,并且不存在,则不做正则判断
+            if(array_key_exists("optional",$arr) && $d["optional"] === true && !array_key_exists($id, $arr)){
+                return false;
+            }
+
             //判断参数是否存在
             if (array_key_exists($id, $arr)) {
                 if (array_key_exists("regex", $d)) {
