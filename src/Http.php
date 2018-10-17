a0ccb7074e1cabf53f33fd7ca62b9fce4a005005
285672259@qq.com <a6470280>
2018-10-17 11:11:43 +0800

    update

--- src/Http.php
+++ src/Http.php
@@ -13,33 +13,45 @@ use Mockery\Exception;
 
 class Http
 {
-    public static function get($url, $headers, $params, $timeout, $times)
+    public static function get($options)
     {
-        return self::request("GET", $url, $headers, $params, $timeout, $times);
+        $options["method"] = "GET";
+        return self::request($options);
     }
 
-    public static function post($url, $headers, $params, $timeout, $times)
+    public static function post($options)
     {
-        return self::request("POST", $url, $headers, $params, $timeout, $times);
+        $options["method"] = "POST";
+        return self::request($options);
     }
 
-    public static function patch($url, $headers, $params, $timeout, $times)
+    public static function patch($options)
     {
-        return self::request("PATCH", $url, $headers, $params, $timeout, $times);
+        $options["method"] = "PATCH";
+        return self::request($options);
     }
 
-    public static function delete($url, $headers, $params, $timeout, $times)
+    public static function delete($options)
     {
-        return self::request("DELETE", $url, $headers, $params, $timeout, $times);
+        $options["method"] = "DELETE";
+        return self::request($options);
     }
 
-    public static function put($url, $headers, $params, $timeout, $times)
+    public static function put($options)
     {
-        return self::request("PUT", $url, $headers, $params, $timeout, $times);
+        $options["method"] = "PUT";
+        return self::request($options);
     }
 
-    public static function request($method, $url, $headers = ["Accept" => "application/json; charset=utf-8"], $params, $timeout = 10, $times = 1)
+    public static function request($options)
     {
+        $defaultOptions = [
+            "headers" => ["Accept" => "application/json; charset=utf-8"],
+            "timeout" => 10,
+            "times" => 1
+        ];
+        $options = array_merge($defaultOptions, $options);
+        ["method" => $method, "url" => $url, "headers" => $headers, "params" => $params, "timeout" => $timeout, "times" => $times] = $options;
         $client = new Client();
         $status = 408;
         $message = "";
@@ -58,8 +70,9 @@ class Http
             }
         } catch (Exception $e) {
             $times--;
+            $options["times"] = $times;
             if ($times > 0) {
-                return self::request($method, $url, $headers, $params, $timeout, $times);
+                return self::request($options);
             }
         }
         return ["status" => $status, "message" => $message];
